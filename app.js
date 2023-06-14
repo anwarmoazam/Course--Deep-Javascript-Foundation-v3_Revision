df895f452dd8d948e59525c18f71adfaa89d90cd
anwarmoazam <anwar.palimarwar@gmail.com>
2023-06-15 00:31:36 +0530

    course deep js

--- app.js
+++ app.js
@@ -62,15 +62,15 @@ console.log(trendRate.toString());
 console.log(trendRate === 0);
 console.log(trendRate < 0);
 console.log(trendRate > 0);
-console.log(Object.is(trendRate,-0));
-console.log(Object.is(trendRate,0));
+console.log(Object.is(trendRate, -0));
+console.log(Object.is(trendRate, 0));
 console.log(Math.sign(-3));
 console.log(Math.sign(3));
 console.log(Math.sign(-0));
 console.log(Math.sign(0));
 
-function formatTrend(trendRate){
-    var direction = (trendRate < 0 || Object.is(trendRate,-0)) ? "▼" : "▲";
+function formatTrend(trendRate) {
+    var direction = (trendRate < 0 || Object.is(trendRate, -0)) ? "▼" : "▲";
     return `${direction} ${Math.abs(trendRate)}`;
 }
 
@@ -80,27 +80,47 @@ console.log(formatTrend(-0));
 console.log(formatTrend(0));
 
 // TODO : define polyfill for `Object.is(..)`
+if (!Object.is || true) {
+    Object.is = function ObjectIs(x, y) {
+        var xNegZero = isItNegZero(x);
+        var yNegZero = isItNegZero(y);
 
+        if (xNegZero || yNegZero) {
+            return xNegZero && yNegZero;
+        } else if (isItNaN(x) && isItNaN(y)) {
+            return true;
+        } else {
+            return x === y;
+        }
 
+        function isItNegZero(v) {
+            return v == 0 && (1 / v) == -Infinity;
+        }
+
+        function isItNaN(v) {
+            return v !== v;
+        }
+    }
+}
 
 // tests
-console.log(Object.is(42,42) === true);
-console.log(Object.is("foo","foo") === true);
-console.log(Object.is(false,false) === true);
-console.log(Object.is(null,null) === true);
-console.log(Object.is(undefined,undefined) === true);
-console.log(Object.is(NaN,NaN) === true);
-console.log(Object.is(-0,-0) === true);
-console.log(Object.is(0,0) === true);
+console.log(Object.is(42, 42) === true);
+console.log(Object.is("foo", "foo") === true);
+console.log(Object.is(false, false) === true);
+console.log(Object.is(null, null) === true);
+console.log(Object.is(undefined, undefined) === true);
+console.log(Object.is(NaN, NaN) === true);
+console.log(Object.is(-0, -0) === true);
+console.log(Object.is(0, 0) === true);
 
-console.log(Object.is(-0,0) === false);
-console.log(Object.is(0,-0) === false);
-console.log(Object.is(0,NaN) === false);
-console.log(Object.is(NaN,0) === false);
-console.log(Object.is(42,"42") === false);
-console.log(Object.is("42",42) === false);
-console.log(Object.is("foo","bar") === false);
-console.log(Object.is(false,true) === false);
-console.log(Object.is(null,undefined) === false);
-console.log(Object.is(undefined,null) === false);
+console.log(Object.is(-0, 0) === false);
+console.log(Object.is(0, -0) === false);
+console.log(Object.is(0, NaN) === false);
+console.log(Object.is(NaN, 0) === false);
+console.log(Object.is(42, "42") === false);
+console.log(Object.is("42", 42) === false);
+console.log(Object.is("foo", "bar") === false);
+console.log(Object.is(false, true) === false);
+console.log(Object.is(null, undefined) === false);
+console.log(Object.is(undefined, null) === false);
 
